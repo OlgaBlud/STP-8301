de66df4771fe368a32861cb7e40e98c967b9df48
Yuliia Hararshchenko <yharashchenko@ukr.net>
2025-03-19 09:15:45 +0200

    faq icons hover/focus

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,12 +1,5 @@
 import Accordion from 'accordion-js';
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   new Accordion('.accordion-container', {
-//     duration: 600,
-//     showMultiple: true,
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
   new Accordion('.accordion-container', {
     duration: 400,
